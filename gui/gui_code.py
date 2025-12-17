de2723305800a35e0ab4a4aa8945ba04fefb510e
Akshaj Goel <akshajgoel@bnpsramvihar.edu.in>
2025-12-17 16:36:28 +0530

    Enhance CoffeeApp with employee features and refactor
    Refactor CoffeeApp to include employee mode and refill functionality. Update screen management and add new features for employee interactions.

--- gui/gui_code.py
+++ gui/gui_code.py
@@ -1,16 +1,15 @@
 import ttkbootstrap as ttk
-from ttkbootstrap import Label, Button
+from ttkbootstrap import Button, Toplevel, StringVar
+from ttkbootstrap.constants import *
 from PIL import Image, ImageTk
 import os, sys, webbrowser
-# 1. Get the directory where gui.py lives
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# 2. Go up one level to the project root ('CoffeeMachine' folder)
 parent_dir = os.path.dirname(current_dir)
-
-# 3. Add that root directory to python's search path
 sys.path.append(parent_dir)
-from backend import verify_resources, machine, process_payment
+from backend import verify_resources, machine, process_payment, speak
+from .employee_verification import EmployeeCodeDialog
+from .passcode_editor import PasscodeEditor
 
 current_script_path = os.path.abspath(__file__)
 gui_folder_path = os.path.dirname(current_script_path)
@@ -26,6 +25,7 @@ class CoffeeApp(ttk.Window):
         self.resizable(True, True)
 
         self.drink=""
+        self.employee_mode=False
 
         # Set icon ONCE for the whole app
         icon_path = os.path.join(project_root, "assets", "logo.ico")
@@ -70,23 +70,78 @@ class CoffeeApp(ttk.Window):
         delivery_page = GiveDrinkPage(self)
         delivery_page.pack(fill="both", expand=True)
 
+    def manage_employee_mode(self):
+        if self.employee_mode:
+            self.employee_mode=False
+            speak("Employee mode disabled.")
+        else:
+            EmployeeCodeDialog(self)
 
-# --- SCREEN 1: START (Converted to Frame) ---
-class StartPage(ttk.Frame):
-    def __init__(self, master):
+    def refill_machine(self):
+        if self.employee_mode:
+            machine.refill()
+            speak("Machine refilled.")
+        else:
+            EmployeeCodeDialog(self, callback=self.refill_machine)
+            speak("Please call an employee to refill the machine!")
+
+#--- Basic SCREEN class ---
+class Screen(ttk.Frame):
+    def __init__(self, master, screen):
         super().__init__(master)
-        self.master = master  # Save reference to the App controller
+        self.focus_set()
 
-        # Logic to load image
+        self.master = master
+
         self.canvas = ttk.Canvas(self)
         self.canvas.pack(fill="both", expand=True)
 
-        screen3_path = os.path.join(project_root, "assets", "StartScreen.png")
+        # Logic to load image
+        screen3_path = os.path.join(project_root, "assets", screen)
         pil_image = Image.open(screen3_path)
         pil_image = pil_image.resize((540, 960), Image.Resampling.LANCZOS)
         self.photo = ImageTk.PhotoImage(pil_image)
 
         self.canvas.create_image(0, 0, image=self.photo, anchor="nw")
+
+        self.bind("<e>", lambda e: self.master.manage_employee_mode())
+        self.bind("<r>", lambda e: self.master.refill_machine())
+
+        self.mail = self.canvas.create_text(
+            187, 840,
+            text="akshajgoel@bnpsramvihar.edu.in",
+            fill="#FFF3DC",
+            font=("Segoe UI", 8)
+        )
+
+        self.web_link = self.canvas.create_text(
+            430, 840,
+            text="aksweb.me",
+            fill="#FFF3DC",
+            font=("Segoe UI", 8)
+        )
+
+        def mail(event):
+            webbrowser.open_new("mailto:akshajgoel@bnpsramvihar.edu.in")
+
+        def open_link(event):
+            webbrowser.open_new("https://aksweb.me")
+
+        self.canvas.tag_bind(self.mail, "<Button-1>", mail)
+        self.canvas.tag_bind(self.mail, "<Enter>", lambda e: self.canvas.config(cursor="hand2"))
+        self.canvas.tag_bind(self.mail, "<Leave>", lambda e: self.canvas.config(cursor=""))
+
+        self.canvas.tag_bind(self.web_link, "<Button-1>", open_link)
+        self.canvas.tag_bind(self.web_link, "<Enter>", lambda e: self.canvas.config(cursor="hand2"))
+        self.canvas.tag_bind(self.web_link, "<Leave>", lambda e: self.canvas.config(cursor=""))
+
+
+# --- SCREEN 1: START (Converted to Frame) ---
+class StartPage(Screen):
+    def __init__(self, master):
+        super().__init__(master, "StartScreen.png")
+
+        speak("Welcome, to the A.K.S Cafe")
 
         btn_order = Button(
             self,
@@ -107,52 +162,38 @@ class StartPage(ttk.Frame):
         )
         self.canvas.create_window(270, 600, window=btn_exit)
 
-        self.mail = self.canvas.create_text(
-            187, 840,
-            text="akshajgoel@bnpsramvihar.edu.in",
-            fill="#FFF3DC",
-            font=("Segoe UI", 8)
+        self.btn_passcode_change = Button(
+            self,
+            text="Change Employee Passcode",
+            bootstyle="warning",
+            width=30,
+            command=lambda e: PasscodeEditor(self.master)
         )
 
-        self.web_link = self.canvas.create_text(
-            430, 840,
-            text="aksweb.me",
-            fill="#FFF3DC",
-            font=("Segoe UI", 8)
-        )
+        self.bind("<e>", lambda e: self.custom_employee_manage())
+        self.bind("<p>", lambda e: PasscodeEditor(self.master))
 
-        def mail(event):
-            webbrowser.open_new("mailto:akshajgoel@bnpsramvihar.edu.in")
+    def custom_employee_manage(self):
+        if self.master.employee_mode:
+            self.master.employee_mode=False
+            speak("Employee mode disabled.")
+        else:
+            EmployeeCodeDialog(self.master, callback=self.passcode_editor)
 
-        def open_link(event):
-            webbrowser.open_new("https://aksweb.me")
-
-        self.canvas.tag_bind(self.mail, "<Button-1>", mail)
-        self.canvas.tag_bind(self.mail, "<Enter>", lambda e: self.canvas.config(cursor="hand2"))
-        self.canvas.tag_bind(self.mail, "<Leave>", lambda e: self.canvas.config(cursor=""))
-
-        self.canvas.tag_bind(self.web_link, "<Button-1>", open_link)
-        self.canvas.tag_bind(self.web_link, "<Enter>", lambda e: self.canvas.config(cursor="hand2"))
-        self.canvas.tag_bind(self.web_link, "<Leave>", lambda e: self.canvas.config(cursor=""))
+    def passcode_editor(self):
+        if self.master.employee_mode:
+            self.canvas.create_window(270, 650, window=self.btn_passcode_change)
+        else:
+            if self.btn_passcode_change.winfo_exists():
+                self.btn_passcode_change.destroy()
 
 
 # --- SCREEN 2: ORDER (Converted to Frame) ---
-class OrderPage(ttk.Frame):
+class OrderPage(Screen):
     def __init__(self, master):
-        super().__init__(master)
-        self.master = master
+        super().__init__(master, "OrderWindow.png")
 
-        self.drink = None
-
-        self.canvas = ttk.Canvas(self)
-        self.canvas.pack(fill="both", expand=True)
-
-        screen3_path = os.path.join(project_root, "assets", "OrderWindow.png")
-        pil_image = Image.open(screen3_path)
-        pil_image = pil_image.resize((540, 960), Image.Resampling.LANCZOS)
-        self.photo = ImageTk.PhotoImage(pil_image)
-
-        self.canvas.create_image(0, 0, image=self.photo, anchor="nw")
+        speak("Which coffee would you like?")
 
         # Buttons
         btn_espresso = Button(self, text="Espresso ($1.50)", bootstyle="warning", width=20,
@@ -167,66 +208,26 @@ class OrderPage(ttk.Frame):
                                 command=lambda: self.select_drink("cappuccino"))
         self.canvas.create_window(270, 650, window=btn_cappuccino)
 
-        self.mail = self.canvas.create_text(
-            187, 840,
-            text="akshajgoel@bnpsramvihar.edu.in",
-            fill="#FFF3DC",
-            font=("Segoe UI", 8)
-        )
-
-        self.web_link = self.canvas.create_text(
-            430, 840,
-            text="aksweb.me",
-            fill="#FFF3DC",
-            font=("Segoe UI", 8)
-        )
-
-        def mail(event):
-            webbrowser.open_new("mailto:akshajgoel@bnpsramvihar.edu.in")
-
-        def open_link(event):
-            webbrowser.open_new("https://aksweb.me")
-
-        self.canvas.tag_bind(self.mail, "<Button-1>", mail)
-        self.canvas.tag_bind(self.mail, "<Enter>", lambda e: self.canvas.config(cursor="hand2"))
-        self.canvas.tag_bind(self.mail, "<Leave>", lambda e: self.canvas.config(cursor=""))
-
-        self.canvas.tag_bind(self.web_link, "<Button-1>", open_link)
-        self.canvas.tag_bind(self.web_link, "<Enter>", lambda e: self.canvas.config(cursor="hand2"))
-        self.canvas.tag_bind(self.web_link, "<Leave>", lambda e: self.canvas.config(cursor=""))
-
     def select_drink(self, drink_name):
         is_enough, missing_item = verify_resources(drink_name)
 
         if is_enough:
             # Look up cost from backend machine data
             cost = machine.MENU[drink_name]["cost"]
-            self.drink = drink_name.title()
-            self.master.drink=self.drink
+            self.master.drink=drink_name.title()
             # Tell controller to switch screens
             self.master.show_payment_screen(cost)
         else:
-            error_text = f"Sorry! Not enough {missing_item}."
-            lbl_error = Label(self, text=error_text, bootstyle="danger", font=("Segoe UI", 12, "bold"))
-            lbl_error.pack(side="bottom", pady=20)
+            speak(f"Sorry! Not enough {missing_item} in the machine.")
 
 
 # --- SCREEN 3: PAYMENT (Converted to Frame) ---
-class PaymentPage(ttk.Frame):
+class PaymentPage(Screen):
     def __init__(self, master, cost):
-        super().__init__(master)
-        self.master = master
+        super().__init__(master, "PaymentWindow.png")
         self.cost = cost
 
-        self.canvas = ttk.Canvas(self)
-        self.canvas.pack(fill="both", expand=True)
-
-        screen3_path = os.path.join(project_root, "assets", "PaymentWindow.png")
-        pil_image = Image.open(screen3_path)
-        pil_image = pil_image.resize((540, 960), Image.Resampling.LANCZOS)
-        self.photo = ImageTk.PhotoImage(pil_image)
-
-        self.canvas.create_image(0, 0, image=self.photo, anchor="nw")
+        speak(f"Please pay ${cost}")
 
         self.canvas.create_text(
             270, 420,
@@ -236,11 +237,11 @@ class PaymentPage(ttk.Frame):
         )
 
         # Spinboxes
-        self.pennies = ttk.Spinbox(self, from_=0, to=100, width=5, bootstyle="warning")
-        self.nickels = ttk.Spinbox(self, from_=0, to=100, width=5, bootstyle="warning")
-        self.dimes = ttk.Spinbox(self, from_=0, to=100, width=5, bootstyle="warning")
-        self.quarters = ttk.Spinbox(self, from_=0, to=100, width=5, bootstyle="warning")
-        self.dollars = ttk.Spinbox(self, from_=0, to=100, width=5, bootstyle="warning")
+        self.pennies = ttk.Spinbox(self, from_=0, to=300, width=5, bootstyle="warning")
+        self.nickels = ttk.Spinbox(self, from_=0, to=60, width=5, bootstyle="warning")
+        self.dimes = ttk.Spinbox(self, from_=0, to=30, width=5, bootstyle="warning")
+        self.quarters = ttk.Spinbox(self, from_=0, to=12, width=5, bootstyle="warning")
+        self.dollars = ttk.Spinbox(self, from_=0, to=3, width=5, bootstyle="warning")
 
         coins = [("Pennies", self.pennies), ("Nickels", self.nickels), ("Dimes", self.dimes),
                  ("Quarters", self.quarters), ("Dollars", self.dollars)]
@@ -267,87 +268,31 @@ class PaymentPage(ttk.Frame):
                          command=lambda: self.pay())
         self.canvas.create_window(270, 600, window=btn_pay)
 
-        self.mail = self.canvas.create_text(
-            187, 840,
-            text="akshajgoel@bnpsramvihar.edu.in",
-            fill="#FFF3DC",
-            font=("Segoe UI", 8)
-        )
-
-        self.web_link = self.canvas.create_text(
-            430, 840,
-            text="aksweb.me",
-            fill="#FFF3DC",
-            font=("Segoe UI", 8)
-        )
-
-        def mail(event):
-            webbrowser.open_new("mailto:akshajgoel@bnpsramvihar.edu.in")
-
-        def open_link(event):
-            webbrowser.open_new("https://aksweb.me")
-
-        self.canvas.tag_bind(self.mail, "<Button-1>", mail)
-        self.canvas.tag_bind(self.mail, "<Enter>", lambda e: self.canvas.config(cursor="hand2"))
-        self.canvas.tag_bind(self.mail, "<Leave>", lambda e: self.canvas.config(cursor=""))
-
-        self.canvas.tag_bind(self.web_link, "<Button-1>", open_link)
-        self.canvas.tag_bind(self.web_link, "<Enter>", lambda e: self.canvas.config(cursor="hand2"))
-        self.canvas.tag_bind(self.web_link, "<Leave>", lambda e: self.canvas.config(cursor=""))
 
     def pay(self):
-        # 1. Get values directly from the Spinboxes
-        # We use 'or 0' to handle cases where the box might be empty
-        try:
-            pennies = int(self.pennies.get() or 0)
-            nickels = int(self.nickels.get() or 0)
-            dimes = int(self.dimes.get() or 0)
-            quarters = int(self.quarters.get() or 0)
-            dollars = int(self.dollars.get() or 0)
-        except ValueError:
-            # Safety net if they type text instead of numbers
-            pennies = nickels = dimes = quarters = dollars = 0
-
-        # 2. Pass these new values to the backend
-        is_enough, change = process_payment(
+        is_enough, change, payment = process_payment(
             self.cost,
-            pennies,
-            nickels,
-            dimes,
-            quarters,
-            dollars
+            self.pennies.get(),
+            self.nickels.get(),
+            self.dimes.get(),
+            self.quarters.get(),
+            self.dollars.get()
         )
-
-        if is_enough:
+        if not payment:
+            speak("Sorry! The machine doesn't have enough money, please enter a lower amount.")
+        elif is_enough and payment:
             if change == 0:
                 self.master.show_delivery_screen()
             else:
                 # We need to pass 'change' to this screen so we can display it!
                 self.master.show_give_change_screen(change)
         else:
-            self.canvas.create_text(
-                270, 700,
-                text=f"Not enough money! Need ${self.cost}",
-                fill="red",
-                font=("Segoe UI", 12, "bold")
-            )
+            speak(f"Not enough money! Please insert ${self.cost-payment} more.")
 
 
-class GiveChangePage(ttk.Frame):
+class GiveChangePage(Screen):
     def __init__(self, master, change):
-        super().__init__(master)
-        self.master = master
-
-        self.canvas = ttk.Canvas(self)
-        self.canvas.pack(fill="both", expand=True)
-
-        # Load your ChangeWindow.png here
-        screen_path = os.path.join(project_root, "assets", "ChangeWindow.png")
-        pil_image = Image.open(screen_path)
-        pil_image = pil_image.resize((540, 960), Image.Resampling.LANCZOS)
-        self.photo = ImageTk.PhotoImage(pil_image)
-        self.canvas.create_image(0, 0, image=self.photo, anchor="nw")
-
+        super().__init__(master, "ChangeWindow.png")
         # Show the Change Amount
         self.canvas.create_text(
             270, 450,
@@ -356,6 +301,8 @@ class GiveChangePage(ttk.Frame):
             font=("Segoe UI", 20, "bold"),
             justify="center"
         )
+
+        speak(f"Here is your change of ${change}.")
 
         # Button to collect change and move to delivery
         btn_collect = Button(
@@ -367,47 +314,10 @@ class GiveChangePage(ttk.Frame):
         )
         self.canvas.create_window(270, 600, window=btn_collect)
 
-        self.mail = self.canvas.create_text(
-            187, 840,
-            text="akshajgoel@bnpsramvihar.edu.in",
-            fill="#FFF3DC",
-            font=("Segoe UI", 8)
-        )
 
-        self.web_link = self.canvas.create_text(
-            430, 840,
-            text="aksweb.me",
-            fill="#FFF3DC",
-            font=("Segoe UI", 8)
-        )
-
-        def mail(event):
-            webbrowser.open_new("mailto:akshajgoel@bnpsramvihar.edu.in")
-
-        def open_link(event):
-            webbrowser.open_new("https://aksweb.me")
-
-        self.canvas.tag_bind(self.mail, "<Button-1>", mail)
-        self.canvas.tag_bind(self.mail, "<Enter>", lambda e: self.canvas.config(cursor="hand2"))
-        self.canvas.tag_bind(self.mail, "<Leave>", lambda e: self.canvas.config(cursor=""))
-
-        self.canvas.tag_bind(self.web_link, "<Button-1>", open_link)
-        self.canvas.tag_bind(self.web_link, "<Enter>", lambda e: self.canvas.config(cursor="hand2"))
-        self.canvas.tag_bind(self.web_link, "<Leave>", lambda e: self.canvas.config(cursor=""))
-
-
-class GiveDrinkPage(ttk.Frame):
+class GiveDrinkPage(Screen):
     def __init__(self, master):
-        super().__init__(master)
-        self.master = master
-
-        self.canvas = ttk.Canvas(self)
-        self.canvas.pack(fill="both", expand=True)
-        screen_path = os.path.join(project_root, "assets", f"{self.master.drink}Window.png")
-        pil_image = Image.open(screen_path)
-        pil_image = pil_image.resize((540, 960), Image.Resampling.LANCZOS)
-        self.photo = ImageTk.PhotoImage(pil_image)
-        self.canvas.create_image(0, 0, image=self.photo, anchor="nw")
+        super().__init__(master, "DeliveryWindow.png")
 
         self.canvas.create_text(
             280, 370,
@@ -415,6 +325,7 @@ class GiveDrinkPage(ttk.Frame):
             fill="#6D1F00",
             font=("Segoe UI", 24, "bold")
         )
+        speak(f"Enjoy your {self.master.drink}. Thank you for choosing us.")
 
         # Button to restart
         btn_home = Button(
@@ -425,32 +336,3 @@ class GiveDrinkPage(ttk.Frame):
             command=self.master.show_order_screen
         )
         self.canvas.create_window(270, 770, window=btn_home)
-
-        self.mail = self.canvas.create_text(
-            187, 840,
-            text="akshajgoel@bnpsramvihar.edu.in",
-            fill="#FFF3DC",
-            font=("Segoe UI", 8)
-        )
-
-        self.web_link = self.canvas.create_text(
-            430, 840,
-            text="aksweb.me",
-            fill="#FFF3DC",
-            font=("Segoe UI", 8)
-        )
-
-        def mail(event):
-            webbrowser.open_new("mailto:akshajgoel@bnpsramvihar.edu.in")
-
-        def open_link(event):
-            webbrowser.open_new("https://aksweb.me")
-
-        self.canvas.tag_bind(self.mail, "<Button-1>", mail)
-        self.canvas.tag_bind(self.mail, "<Enter>", lambda e: self.canvas.config(cursor="hand2"))
-        self.canvas.tag_bind(self.mail, "<Leave>", lambda e: self.canvas.config(cursor=""))
-
-        self.canvas.tag_bind(self.web_link, "<Button-1>", open_link)
-        self.canvas.tag_bind(self.web_link, "<Enter>", lambda e: self.canvas.config(cursor="hand2"))
-
-        self.canvas.tag_bind(self.web_link, "<Leave>", lambda e: self.canvas.config(cursor=""))
